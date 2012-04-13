74f9aef4f74d3931eba01c32889d4dda4628e64a
A. Matías Quezada <amatiasq@gmail.com>
2012-04-13 19:05:19 +0200

    Developing class tests

--- test/class.test.js
+++ test/class.test.js
@@ -2,3 +2,11 @@
  * Copyright © 2009-2012 A. Matías Quezada
  */
 
+describe("Class object", function() {
+	describe("extend method", function() {
+		it("should return a function who has the extend function", function() {
+			var test = Class.extend();
+			expect(test).toBeFunction();
+		});
+	});
+});

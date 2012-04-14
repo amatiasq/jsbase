13e4b7a7ba4da6e43f683883c35323e081e344e8
A. Matías Quezada <amatiasq@gmail.com>
2012-04-14 15:09:12 +0200

    Emitter test complete

--- test/emitter.test.js
+++ test/emitter.test.js
@@ -3,10 +3,14 @@
  */
 
 describe("Emitter class", function() {
+	var emitter, spy;
+
+	beforeEach(function() {
+		emitter = new Emitter();
+		spy = new sassmine.Spy();
+	});
 
 	describe('on-emit relation', function() {
-		var emitter = new Emitter();
-		var spy = new sassmine.Spy();
 
 		it("should be able to add listeners by the on method", function() {
 			emitter.on('something', spy);
@@ -26,6 +30,47 @@ describe("Emitter class", function() {
 			expect(spy.callCount).toBe(0);
 			emitter.emit('another singal');
 			expect(spy.callCount).toBe(0);
+		});
+
+		it("should fire the listener as many times as it is added", function() {
+			emitter.on('something', spy);
+
+			expect(spy.callCount).toBe(0);
+			emitter.emit('something');
+			expect(spy.callCount).toBe(2);
+		});
+	});
+
+	describe("off method", function() {
+
+		it("must disable a listener for a determined signal", function() {
+			emitter.on('something', spy);
+
+			expect(spy.callCount).toBe(0);
+			emitter.emit('something');
+			emitter.emit('something');
+			expect(spy.callCount).toBe(2);
+
+			emitter.off('something', spy);
+
+			emitter.emit('something');
+			expect(spy.callCount).toBe(2);
+		});
+	});
+
+	describe("once method", function() {
+
+		it("should add a listener to be fired only once", function() {
+			emitter.once('something', spy);
+
+			expect(spy.callCount).toBe(0);
+			emitter.emit('something');
+			expect(spy.callCount).toBe(1);
+			emitter.emit('something');
+			expect(spy.callCount).toBe(1);
+			emitter.emit('something');
+			expect(spy.callCount).toBe(1);
+
 		});
 	});
 });

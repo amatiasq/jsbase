a8b7ae86ce6e0a13f089b32ce9ca515d228ec0a2
A. Matías Quezada <amatiasq@gmail.com>
2012-12-14 02:09:27 +0100

    Added https://github.com/domenic/promise-tests to the test suite

---
+++ test/promise-tests-adapter.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var Promise = require("../src/promise");
+
+exports.fulfilled = Promise.resolved;
+exports.rejected = Promise.rejected;
+
+exports.pending = function () {
+    var deferred = new Promise();
+
+    return {
+        promise: deferred.future,
+        fulfill: deferred.resolve.bind(deferred),
+        reject: deferred.reject.bind(deferred)
+    };
+};
